b25833f3b78381832c829e3a5171f4cbb65d750c
Lopresti Tomas <tomaslopresti09@gmail.com>
2025-04-13 01:57:11 -0300

    conexion funcionando

--- PryLoprestiConexionBD/frmInicio.Designer.cs
+++ PryLoprestiConexionBD/frmInicio.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(809, 450);
             this.Name = "frmInicio";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Conexion a Base de Datos";
