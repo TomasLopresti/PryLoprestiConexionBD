94f48f4470e2263b682c0eb0d615fc58a13965b9
tomas lopresti <tomaslopresti09@gmail.com>
2025-04-14 11:18:38 -0300

    todo funcionando

--- PryLoprestiConexionBD/frmInicio.Designer.cs
+++ PryLoprestiConexionBD/frmInicio.Designer.cs
@@ -44,7 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(775, 450);
+            this.ClientSize = new System.Drawing.Size(770, 450);
             this.Controls.Add(this.dgvConexion);
             this.Name = "frmInicio";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
